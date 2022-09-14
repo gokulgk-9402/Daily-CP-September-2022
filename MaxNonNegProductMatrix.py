c8dc7536a5f05aba73e01d31161e26694888bae4
gokulgk-9402 <gokulgk9402@gmail.com>
2022-09-14 21:52:22 +0530

    Problems for September 14, 2022

--- MaxNonNegProductMatrix.py
+++ MaxNonNegProductMatrix.py
@@ -5,21 +5,29 @@ class Solution:
         
         m = len(grid)
         n = len(grid[0])
+        
+        maximums = [["#" for _ in range(n)] for _ in range(m)]
+        minimums = [["#" for _ in range(n)] for _ in range(m)]
+        
+        maximums[m-1][n-1] = grid[-1][-1]
+        minimums[m-1][n-1] = grid[-1][-1]
 
-        mem = {}
-        mem[(m-1, n-1)] = (grid[-1][-1], grid[-1][-1])
         for i in range(n-1, 0, -1):
-            mem[(m-1, i-1)] = grid[m-1][i-1] * mem[(m-1, i)][0], grid[m-1][i-1] * mem[(m-1, i)][0]
+            maximums[m-1][i-1] = grid[m-1][i-1] * maximums[m-1][i]
+            minimums[m-1][i-1] = grid[m-1][i-1] * minimums[m-1][i]
+            
         for i in range(m-1, 0, -1):
-            mem[(i-1, n-1)] = grid[i-1][n-1] * mem[(i, n-1)][0], grid[i-1][n-1] * mem[(i, n-1)][0]
+            maximums[i-1][n-1] = grid[i-1][n-1] * maximums[i][n-1]
+            minimums[i-1][n-1] = grid[i-1][n-1] * minimums[i][n-1]
 
-        # print(mem)
+        # print(maximums)
+        # print(minimums)
 
         def recursion(x, y):
             # print(x, y)
 
-            if (x,y) in mem:
-                return mem[(x,y)]
+            if maximums[x][y] != '#' and minimums[x][y] != '#':
+                return maximums[x][y], minimums[x][y]
 
             max1, min1 = recursion(x + 1, y)
             # print(x+1, y, max1, min1)
@@ -27,15 +35,19 @@ class Solution:
             max2, min2 = recursion(x, y + 1)
             # print(x, y+1, max2, min2)
 
+            # print(max1, max2, min1, min2)
+
             maximum = max(max1, max2, min1, min2)
             minimum = min(max1, max2, min1, min2)
 
             if grid[x][y] < 0:
-                mem[(x,y)] = grid[x][y] * minimum, grid[x][y] * maximum
-                return mem[(x,y)]
+                maximums[x][y] = grid[x][y] * minimum
+                minimums[x][y] = grid[x][y] * maximum
+                return maximums[x][y], minimums[x][y]
             else:
-                mem[(x,y)] = grid[x][y] * maximum, grid[x][y] * minimum
-                return mem[(x,y)]
+                maximums[x][y] = grid[x][y] * maximum
+                minimums[x][y] = grid[x][y] * minimum
+                return maximums[x][y], minimums[x][y]
 
         val = max(recursion(0, 0))
         if val < 0:
